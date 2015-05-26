6bacd6656ae6a78a73b256287212fa37309f6c71
Vicky Vernando Dasta <vickydasta@ymail.com>
2015-05-26 19:23:17 +0700

    Create personalpronoment.py

---
+++ kernel/personalpronoment.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+#personal pronoment in english
+#usage :
+#>>> from personalpronoment import *
+#>>> x=personal()
+#>>> x.pronoment[0]
+#>>> 'i'
+#and so on
+
+class personal:
+    pronoment=['i','you','she','he','it','we','they']
