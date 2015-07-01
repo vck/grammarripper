3f572e8c8d8c4f9350b61f6e04611ca9a2e81487
vickydasta <vickydasta@ymail.com>
2015-07-01 08:29:51 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+__author__ = 'vickydasta'
+__app__ = 'grammar ripper'
+
+'''
+---------------
+Grammar Ripper
+---------------
+a confuser machine to shuffle your sentence and will make your grammar looking bad
+'''
+#starting engine
+# grammaripper/kernel/* --> grammar ripper module
+# grammaripper/kernel/tobe.py --> list of tobe in English obviously
+# grammaripper/kernel/procword.py --> word processor
+
+from kernel.core import *
+import logging
+
+while True:
+    try:
+        sentence = raw_input('new sentence .> ')
+        print '-> ',sentence2word(sentence)
+        print '-> ',ripthegrammar(ripp(sentence))
+    except KeyboardInterrupt:
+        print '\ninterrupted by user'
+        exit()
+    except Exception as e:
+        logging.error(e)
